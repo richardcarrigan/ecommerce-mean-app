b5252d973a56550dbc54d9cbe391db1dbacd40f9
nihonjinboy85 <nihonjinboy85@gmail.com>
2018-10-01 12:10:23 -0700

    Set static folder for deployment

--- app.js
+++ app.js
@@ -1,34 +1,42 @@
 const express = require('express'),
+      path = require('path'),
       mongoose = require('mongoose'),
       bodyParser = require('body-parser'),
       config = require('./config/database'),
-      cors = require('cors'),
-      app = express();
+      cors = require('cors');
+
+const app = express();
 
 mongoose.Promise = global.Promise;
 
+// Connect to Database
 mongoose.connect(config.database, {useNewUrlParser: true}, (err) => {
   err? console.log(err) : console.log('Connected to DB: ' + config.database);
 });
 
+// Set server host and port
 const host = '0.0.0.0';
 const port = process.env.PORT || 8080;
+
 const productRoutes = require('./routes/products');
 
+// Body Parser Middleware
 app.use(bodyParser.json());
 
+// CORS Middleware
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.render('./dist/index.html');
-});
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'dist')));
 
+// API Routes
 app.use('/products', productRoutes);
 
 app.get('*', (req, res) => {
   res.render('./dist/index.html');
 });
 
+// Start Server
 app.listen(port, host, () => {
   console.log('Server listening on port ' + port + '...');
 });

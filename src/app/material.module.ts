49ec77fd7f9a7913a0567a609223fdb7296c4dc3
Richard Carrigan <nihonjinboy85@gmail.com>
2020-09-01 22:51:47 -0700

    Update @angular/material imports

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
-
-import {
-  MatSidenavModule,
-  MatToolbarModule,
-  MatIconModule,
-  MatListModule,
-  MatButtonModule,
-  MatCardModule
-} from '@angular/material';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   imports: [

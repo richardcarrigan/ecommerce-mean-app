c346f3eb06aa11c038060a271a910ad18d67af95
Richard Carrigan <nihonjinboy85@gmail.com>
2020-11-01 11:35:13 -0800

    Migrate db to MongoDB Atlas, Install dotenv

--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-  database: 'mongodb://richard:password@ds155577.mlab.com:55577/ecommerce-mean-app'
+  database: 'mongodb+srv://richard:password@ecommerce-mean-app.is6ed.mongodb.net/ecommerce-mean-app?retryWrites=true&w=majority'
 }
